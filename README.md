aa0810c61ea44b06d9337feffafc8bc6c5a8ddf2
temaisgod <perezz.mario@gmail.com>
2020-09-16 11:25:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-9-material-starter-zjv3hz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-9-material-starter-zjv3hz)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+}
